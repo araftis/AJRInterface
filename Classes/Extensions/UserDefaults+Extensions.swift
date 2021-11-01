a15a4b3d5e7bc2ff23b647a50ac4562e27afcd56
AJ Raftis <raftisalex@gmail.com>
2021-11-01 16:58:04 -0700

    Got "structure inspectors" working. These replace the layer inspector and add panels for object structure and messages. Note that the latter two are not yet implemented, but just place holders.

--- Classes/Extensions/UserDefaults+Extensions.swift
+++ Classes/Extensions/UserDefaults+Extensions.swift
@@ -6,6 +6,7 @@
 //
 
 import AJRFoundation
+import AppKit
 
 extension NSFont : AJRUserDefaultProvider {
     
@@ -16,6 +17,19 @@ extension NSFont : AJRUserDefaultProvider {
     
     public static func setUserDefault(_ value: NSFont?, forKey key: String, into userDefaults: UserDefaults) {
         userDefaults.set(value == nil ? nil : AJRStringFromFont(value!), forKey:key)
+    }
+    
+}
+
+extension NSUserInterfaceItemIdentifier : AJRUserDefaultProvider {
+    
+    public static func userDefault(forKey key: String, from userDefaults: UserDefaults) -> NSUserInterfaceItemIdentifier? {
+        let string = userDefaults.string(forKey:key)
+        return string == nil ? nil : NSUserInterfaceItemIdentifier(string!)
+    }
+    
+    public static func setUserDefault(_ value: NSUserInterfaceItemIdentifier?, forKey key: String, into userDefaults: UserDefaults) {
+        userDefaults.set(value?.rawValue, forKey:key)
     }
     
 }
